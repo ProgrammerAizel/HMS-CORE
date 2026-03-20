6b1784dfff232803d29f630aca1ebe21b23ef327
Programmer Aizel <alipzacreations1986@gmail.com>
2026-03-20 17:10:59 +0530

    Fix: Connect to Atlas using env variable

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,8 @@ var mongoose = require('mongoose')
 
 let connectDb = async ()=>{
     try{
-        await mongoose.connect('mongodb://127.0.0.1:27017/HMS');
+const url = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/HMS';
+await mongoose.connect(url);
         console.log("Database connected succesfully");
     }catch(err){
         console.log("database error: ",err)
